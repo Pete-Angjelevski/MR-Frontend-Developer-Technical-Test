1fd703536da76933c29a71c4950da6fb722df106
Pete Angjelevski <p.angjelevski@gmail.com>
2021-05-19 15:33:57 +1200

    fill cart component

--- src/client/components/Cart.tsx
+++ src/client/components/Cart.tsx
@@ -1,4 +1,9 @@
 import * as React from 'react'
+import { CartType } from '../types/appSpecificTypes' 
+import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
+
+
 
 
 interface ICartProps {
@@ -8,11 +13,28 @@ interface ICartProps {
 
 const Cart: React.FC<ICartProps> = () => {
   
+  const cart = useSelector((state: any) => state.cart)
+
+  
+
+
   return (
-    <div></div>
+    <div>
+      <ul>
+        {cart.map((item: CartType) => {
+          return (
+            <li>
+              <img src={item.img} />
+              <p>{item.name}</p>
+              <p>{item.quantity}x <strong>${item.price}.00</strong> </p>
+              <p>Size: {item.size}</p>
+            </li>)
+        })}
+      </ul>
+    </div>
     
     )
 }
 
 
-export default Cart
+export default connect()(Cart)
