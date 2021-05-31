008ee2a327a06d7a2c55812cc7f74d569018ef3b
Pete Angjelevski <p.angjelevski@gmail.com>
2021-05-31 17:23:58 +1200

    add cart count

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, Route } from 'react-router-dom'
-
+import { connect, useSelector } from 'react-redux'
 
 //COMPONENTS
 import Cart from './Cart'
@@ -10,21 +10,39 @@ import Cart from './Cart'
 // STYLES
 import "../scss/Header.scss"
 
-
+// TYPES
+import { CartType } from '../types/appSpecificTypes' 
 
 interface IHeaderProps {
 
 }
 
 const Header: React.FC<IHeaderProps> = () => {
+
+  const cart = useSelector((state: any) => state.cart)
+
+  const [cartCount, setcartCount ] = useState<number>(0)
   const [trigger, setTrigger ] = useState<boolean>(false)
+
+
+  useEffect(() => {
+    let count: number = 0
+    
+    cart.forEach((item: CartType) => {
+      count += item.quantity
+    })
+
+    setcartCount(count) 
+    
+  }, [cart, cartCount])
+
 
    
 
 
     return (
         <div className="headerContainer">
-          <Link className="link" onClick={() => setTrigger(!trigger)} to={trigger ? '/cart' : '/'}>My Cart(0)</Link>
+          <Link className="link" onClick={() => setTrigger(!trigger)} to={trigger ? '/cart' : '/'}>My Cart({cartCount})</Link>
           <Route path="/cart" component={Cart} />
           
         </div>
@@ -32,4 +50,4 @@ const Header: React.FC<IHeaderProps> = () => {
           )
 }
 
-export default Header
+export default connect()(Header)
